52bf1b3dc32c27417a5e1798d4e8ce3adf2184e3
shivam-mechcode <70307820+shivam-mechcode@users.noreply.github.com>
2022-09-12 22:14:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const fs = require('fs')
+const http = require('http')
+const url = require('url')
+const replaceTemplate = require('./dev-data/modules/replaceTemplate')
+const slugify = require('slugify')
+
+// Synchronous Code
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+// console.log(textIn)
+// const textOut = `This is what we know about avacado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File Written')
+
+
+// Asynchronous code
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
+//     if(err) return(console.log('ERROR'))
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3)=>{
+//     console.log(data3)
+//     fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8' ,err=>{
+//         console.log('Your file has been written')
+//     })
+// });
+    
+// });
+// })
+
+//////////////////////////////////////////////////
+//SERVER
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+const slugs = dataObj.map(el => slugify(el.productName, {lower:true}))
+
+console.log(slugs)
+const server = http.createServer((req, res)=>{
+    // console.log(req.url)
+    const {query, pathname} = url.parse(req.url, true)
+    // const pathName = req.url;
+
+    //Overview Page
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {
+                'Content-type': 'text/html'
+            })
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('')  
+        const output = tempOverview.replace('{%PRODUCT_CARD%', cardsHtml)
+         return res.end(output);
+
+         return res.end(tempOverview);
+    }
+
+    //Prduct Page
+    else if(pathname === '/product'){
+        const product = dataObj[query.id];
+        res.writeHead(200, {
+                'Content-type': 'text/html'
+            })
+        const output = replaceTemplate(tempProduct, product)
+        return res.end(output)
+
+    }
+
+    // API
+    else if(pathname === '/api'){
+       res.writeHead(200, {
+                'Content-type': 'application/json'
+            })
+           return res.end(data)
+        
+    }
+
+    //Not Found
+    else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        return res.end('<h1>Page Not Found</h1>')
+    }
+    // res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('Listening to the request on port 8000')
+});
+
+
+
+
+ 
